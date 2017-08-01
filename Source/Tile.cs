c57c250f0899744581243ef50f42daeab077d8f4
Balaji Veeramani <bveeramani@aol.com>
2017-07-31 21:10:18 -0500

    Add files via upload

---
+++ Source/Tile.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Tile : MonoBehaviour {
+
+	public enum Type {Default, Stage, Level, Ice, Portal, Drain, Fire, Door};
+	public Type type = Type.Default;
+
+	public GameObject flame;
+	public GameObject exit;
+	public bool startsOff = true;
+	private int oldTurn = 0;
+	private bool flagged = false;
+	private new Renderer renderer;
+
+	private bool locked = true;
+
+	private void Start () {
+		renderer = GetComponent<Renderer>();
+	}
+
+	private void Update() {
+		gameObject.name = gameObject.tag;
+		if (type == Type.Default) {
+			gameObject.tag = "Default Tile";
+			switch (Game.index + 1) {
+				case 1:
+					//renderer.material = (Material)Resources.Load("Materials/Pastel/Orange");
+					break;
+				case 2:
+					//renderer.material = (Material)Resources.Load("Materials/Grey");
+					break;
+				case 3:
+					//renderer.material = (Material)Resources.Load("Materials/DarkGrey");
+					break;
+			}
+		}
+		if (type == Type.Stage) {
+			gameObject.tag = "Stage Tile";
+			renderer.material = (Material)Resources.Load("Materials/Pastel/Red");
+		}
+		if (type == Type.Level) {
+			gameObject.tag = "Level Tile";
+			renderer.material = (Material)Resources.Load("Materials/Pastel/Red");
+		}
+		if (type == Type.Ice) {
+			gameObject.tag = "Ice Tile";
+			renderer.material = (Material)Resources.Load("Materials/Pastel/Blue");
+		}
+		if (type == Type.Portal) {
+			gameObject.tag = "Portal Tile";
+			//renderer.material = (Material)Resources.Load("Materials/Ring");
+		}
+		if (type == Type.Fire) {
+			if (oldTurn != Stage.turn) {
+				oldTurn = Stage.turn;
+				flagged = false;
+			}
+			if ((Stage.turn % 2 == 1) == startsOff) {
+				renderer.material = (Material)Resources.Load("Materials/Pastel/Orange");
+				if (!flagged) {
+					//Instantiate (flame, transform.position, Quaternion.identity);
+					flagged = true;
+				}
+			} else {
+				renderer.material = (Material)Resources.Load("Materials/White");
+			}
+		}
+		if (type == Type.Door) {
+			gameObject.tag = "Door Tile";
+			int x = (int)transform.position.x;
+			int z = (int)transform.position.z;
+			renderer.material = (Material)Resources.Load("Materials/Pastel/Green");
+			if (locked) {
+				Stage.Disable (x, z);
+			} else {
+				Stage.Enable (x, z);
+				Debug.Log (Stage.Check (x, z));
+				gameObject.SetActive (false);
+			}
+		}
+	}
+
+	private IEnumerator Fade() {
+		float alpha = renderer.material.color.a;
+		while (alpha > 0) {
+			alpha -= 0.03f;
+			Color color = renderer.material.color;
+			color.a = alpha;
+			renderer.material.color = color;
+			yield return null;
+		}
+		Destroy (gameObject);
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			if (type == Type.Fire) {
+				if ((Stage.turn % 2 == 1) == startsOff) {
+					StartCoroutine (Stage.instance.Preload ("Live"));
+				}
+			}
+			if (type == Type.Door) {
+				if (Player.instance.keys > 0) {
+					Player.instance.keys -= 1;
+					locked = false;
+				}
+			}
+		}
+	}
+
+	private void OnTriggerExit (Collider other) {
+		if (other.tag == "Player") {
+			if (type == Type.Ice) {
+				int x = (int)transform.position.x;
+				int z = (int)transform.position.z;
+				Stage.Disable (x, z);
+				StartCoroutine (Fade());
+			}
+		}
+	}
+}
---
+++ Source/Tutorial.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+	public Text text;
+	public static int phase = 0;
+
+	void Start () {
+		phase = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (phase);
+		if ((Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.D)) && phase == 0){
+			StartCoroutine ("FadeOut");
+			phase = 1;
+		}
+		if (phase == 3) {
+			StartCoroutine ("FadeOut");
+		}
+	}
+
+	private IEnumerator FadeOut() {
+		float alpha = text.color.a;
+		alpha = 1;
+		while (alpha > -0.03) {
+			alpha -= 0.015f;
+			text.color = new Color (1, 1, 1, alpha);
+			yield return null;
+		}
+		if (text.text == "Use A & D to rotate camera" && phase == 1 && alpha <= 0) {
+			text.text = "Use arrow keys to move";
+			phase = 2;
+		}
+		if (text.text == "Use arrow keys to move" && phase == 3 && alpha <= 0) {
+			text.text = "Reach the goal to start game";
+			phase = 4;
+		}
+		while (alpha < 1) {
+			alpha += 0.01f;
+			text.color = new Color (1, 1, 1, alpha);
+			yield return null;
+		}
+	}
+}
